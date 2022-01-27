7175a11ffbb4e680fc48d9c1611c61e32ba925a2
rishabhengineer <bindalrishabh97@gmail.com>
2022-01-27 20:15:06 +0530

    My changes

--- ofc-emp-management-system-main/office_emp_proj/emp_app/admin.py
+++ ofc-emp-management-system-main/office_emp_proj/emp_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Employee, Role, Department
+
 # Register your models here.
+from .models import Employee, Role, Department
 
 admin.site.register(Employee)
 admin.site.register(Role)
